68827a875277b321f15c746bb7b4978d3d6938a7
Boukiour <mboukiour@mborijnland.nl>
2023-06-14 12:50:50 +0200

    dbconfig.php toegevoegd,, voor de databaseverbinding

---
+++ dbconfig.php
@@ -0,0 +1,9 @@
+<?php
+
+// Databaseverbinding instellen (vervang 'db_host',
+// 'db_name', 'db_user' en 'db_password' met jouw databasegegevens)
+$host = 'localhost';
+$dbname = 'blog_b';
+$username = 'root';
+$password = '';
+?>
